211e877d2a4b23a259e1de4eaa0d1e5706fa1355
Sebastien LONGO <sebastien.longo@oufy.net>
2021-11-24 21:25:25 +0100

    feat(): update 24/11/2021

--- LOAD IN DCS CATEGORY KEYBOARD.diff.lua
+++ LOAD IN DCS CATEGORY KEYBOARD.diff.lua
@@ -38,6 +38,49 @@ local diff = {
 			},
 			["name"] = "Volume Down",
 		},
+		["d3091pnilu3091cd58vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "Home",
+					["reformers"] = {
+						[1] = "LCtrl",
+					},
+				},
+			},
+			["name"] = "HCU half-action",
+			["removed"] = {
+				[1] = {
+					["key"] = "Home",
+				},
+			},
+		},
+		["d3092pnilu3092cd58vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "Enter",
+				},
+				[2] = {
+					["key"] = "Enter",
+					["reformers"] = {
+						[1] = "LCtrl",
+					},
+				},
+			},
+			["name"] = "HCU full-action",
+			["removed"] = {
+				[1] = {
+					["key"] = "PageUp",
+				},
+			},
+		},
+		["d3094pnilu3094cd58vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "G",
+				},
+			},
+			["name"] = "HCU offset",
+		},
 		["d3096pnilu3096cd58vd1vpnilvu0"] = {
 			["added"] = {
 				[1] = {
@@ -157,12 +200,58 @@ local diff = {
 			["name"] = "Next launch",
 		},
 		["d3391pnilu3391cd2vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "K",
+					["reformers"] = {
+						[1] = "LShift",
+					},
+				},
+			},
 			["name"] = "Mic Button ICS",
 			["removed"] = {
 				[1] = {
 					["key"] = "\\",
 					["reformers"] = {
 						[1] = "RShift",
+					},
+				},
+			},
+		},
+		["d3392pnilu3392cd2vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "\\",
+					["reformers"] = {
+						[1] = "RCtrl",
+					},
+				},
+			},
+			["name"] = "Mic Button UHF",
+			["removed"] = {
+				[1] = {
+					["key"] = "\\",
+					["reformers"] = {
+						[1] = "RAlt",
+					},
+				},
+			},
+		},
+		["d3400pnilunilcd2vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "Num*",
+					["reformers"] = {
+						[1] = "LShift",
+					},
+				},
+			},
+			["name"] = "Mic XMTR SEL Toggle",
+			["removed"] = {
+				[1] = {
+					["key"] = "\\",
+					["reformers"] = {
+						[1] = "RCtrl",
 					},
 				},
 			},
@@ -235,437 +324,16 @@ local diff = {
 			},
 			["name"] = "DDD Interrogate Friend or Foe",
 		},
-		["d3501pnilu3501cd38vd1vpnilvu0"] = {
+		["d3722pnilu3722cd2vd1vpnilvu0"] = {
 			["added"] = {
 				[1] = {
-					["key"] = "H",
-					["reformers"] = {
-						[1] = "RCtrl",
-					},
-				},
-			},
-			["name"] = "LANTIRN Trigger Half Action",
-		},
-		["d3502pnilu3502cd38vd1vpnilvu0"] = {
-			["added"] = {
-				[1] = {
-					["key"] = "F",
-					["reformers"] = {
-						[1] = "RCtrl",
-					},
-				},
-			},
-			["name"] = "LANTIRN Trigger Full Action",
-		},
-		["d3503pnilunilcd38vd1vpnilvu0"] = {
-			["added"] = {
-				[1] = {
-					["key"] = "P",
-					["reformers"] = {
-						[1] = "LAlt",
-					},
-				},
-			},
-			["name"] = "LANTIRN Laser Latched",
-		},
-		["d3504pnilunilcd38vd1vpnilvu0"] = {
-			["added"] = {
-				[1] = {
-					["key"] = "Enter",
-					["reformers"] = {
-						[1] = "RShift",
-					},
-				},
-			},
-			["name"] = "LANTIRN Undesignate",
-		},
-		["d3505pnilu3505cd38vd1vpnilvu0"] = {
-			["added"] = {
-				[1] = {
-					["key"] = "P",
+					["key"] = "Num*",
 					["reformers"] = {
 						[1] = "LCtrl",
 					},
 				},
 			},
-			["name"] = "LANTIRN Point Track",
-		},
-		["d3506pnilu3506cd38vd1vpnilvu0"] = {
-			["added"] = {
-				[1] = {
-					["key"] = "P",
-					["reformers"] = {
-						[1] = "RCtrl",
-					},
-				},
-			},
-			["name"] = "LANTIRN Area Track",
-		},
-		["d3507pnilu3507cd38vd1vpnilvu0"] = {
-			["added"] = {
-				[1] = {
-					["key"] = "Up",
-					["reformers"] = {
-						[1] = "LAlt",
-						[2] = "LCtrl",
-					},
-				},
-			},
-			["name"] = "LANTIRN S4 HAT Up (QADL/QHUD/Code +/Gain+)",
-		},
-		["d3508pnilu3508cd38vd1vpnilvu0"] = {
-			["added"] = {
-				[1] = {
-					["key"] = "Right",
-					["reformers"] = {
-						[1] = "LAlt",
-						[2] = "LCtrl",
-					},
-				},
-			},
-			["name"] = "LANTIRN S4 HAT Right (QDES/Digit Right/Level+)",
-		},
-		["d3509pnilu3509cd38vd1vpnilvu0"] = {
-			["added"] = {
-				[1] = {
-					["key"] = "Down",
-					["reformers"] = {
-						[1] = "LAlt",
-						[2] = "LCtrl",
-					},
-				},
-			},
-			["name"] = "LANTIRN S4 HAT Down (QSNO/Code -/Gain-)",
-		},
-		["d3510pnilunilcd38vd1vpnilvunil"] = {
-			["added"] = {
-				[1] = {
-					["key"] = "U",
-					["reformers"] = {
-						[1] = "LCtrl",
-					},
-				},
-			},
-			["name"] = "LANTIRN QWP-",
-		},
-		["d3511pnilunilcd38vd1vpnilvunil"] = {
-			["added"] = {
-				[1] = {
-					["key"] = "O",
-					["reformers"] = {
-						[1] = "LCtrl",
-					},
-				},
-			},
-			["name"] = "LANTIRN QWP+",
-		},
-		["d3512pnilunilcd38vd1vpnilvu0"] = {
-			["added"] = {
-				[1] = {
-					["key"] = "F",
-				},
-			},
-			["name"] = "LANTIRN Toggle FOV",
-		},
-		["d3513pnilu3513cd38vd1vpnilvu0"] = {
-			["added"] = {
-				[1] = {
-					["key"] = "P",
-					["reformers"] = {
-						[1] = "LAlt",
-						[2] = "LCtrl",
-					},
-				},
-			},
-			["name"] = "LANTIRN Toggle WHOT / BHOT",
-		},
-		["d3514pnilu3514cd38vd-1vpnilvu0"] = {
-			["added"] = {
-				[1] = {
-					["key"] = "F",
-					["reformers"] = {
-						[1] = "LAlt",
-					},
-				},
-			},
-			["name"] = "LANTIRN Slider Laser/Focus",
-		},
-		["d3515pnilu3515cd38vd1vpnilvu0"] = {
-			["added"] = {
-				[1] = {
-					["key"] = "Left",
-					["reformers"] = {
-						[1] = "LAlt",
-						[2] = "LCtrl",
-					},
-				},
-			},
-			["name"] = "LANTIRN S4 HAT Left (Digit Left/Level-)",
-		},
-		["d3516pnilunilcd38vd0vpnilvunil"] = {
-			["added"] = {
-				[1] = {
-					["key"] = "N",
-					["reformers"] = {
-						[1] = "LAlt",
-					},
-				},
-			},
-			["name"] = "LANTIRN Laser Arm SAFE",
-		},
-		["d3516pnilunilcd38vd1vpnilvunil"] = {
-			["added"] = {
-				[1] = {
-					["key"] = "H",
-					["reformers"] = {
-						[1] = "LAlt",
-					},
-				},
-			},
-			["name"] = "LANTIRN Laser Arm ARM",
-		},
-		["d3532pnilu3532cd23vd1vpnilvu0"] = {
-			["added"] = {
-				[1] = {
-					["key"] = "Num-",
-					["reformers"] = {
-						[1] = "LCtrl",
-					},
-				},
-			},
-			["name"] = "CAP S/W/-",
-		},
-		["d3533pnilu3533cd23vd1vpnilvu0"] = {
-			["added"] = {
-				[1] = {
-					["key"] = "Num+",
-					["reformers"] = {
-						[1] = "LCtrl",
-					},
-				},
-			},
-			["name"] = "CAP N/E/+",
-		},
-		["d3534pnilu3534cd23vd1vpnilvu0"] = {
-			["added"] = {
-				[1] = {
-					["key"] = "NumEnter",
-				},
-			},
-			["name"] = "CAP Enter",
-		},
-		["d3535pnilu3535cd23vd1vpnilvu0"] = {
-			["added"] = {
-				[1] = {
-					["key"] = "Num0",
-				},
-			},
-			["name"] = "CAP 0/Bearing",
-		},
-		["d3536pnilu3536cd23vd1vpnilvu0"] = {
-			["added"] = {
-				[1] = {
-					["key"] = "Num1",
-				},
-			},
-			["name"] = "CAP 1/Latitude",
-		},
-		["d3537pnilu3537cd23vd1vpnilvu0"] = {
-			["added"] = {
-				[1] = {
-					["key"] = "Num2",
-				},
-			},
-			["name"] = "CAP 2/Number",
-		},
-		["d3538pnilu3538cd23vd1vpnilvu0"] = {
-			["added"] = {
-				[1] = {
-					["key"] = "Num3",
-				},
-			},
-			["name"] = "CAP 3/Speed",
-		},
-		["d3539pnilu3539cd23vd1vpnilvu0"] = {
-			["added"] = {
-				[1] = {
-					["key"] = "Num4",
-				},
-			},
-			["name"] = "CAP 4/Altitude",
-		},
-		["d3540pnilu3540cd23vd1vpnilvu0"] = {
-			["added"] = {
-				[1] = {
-					["key"] = "Num5",
-				},
-			},
-			["name"] = "CAP 5/Range",
-		},
-		["d3541pnilu3541cd23vd1vpnilvu0"] = {
-			["added"] = {
-				[1] = {
-					["key"] = "Num6",
-				},
-			},
-			["name"] = "CAP 6/Longitude",
-		},
-		["d3542pnilu3542cd23vd1vpnilvu0"] = {
-			["added"] = {
-				[1] = {
-					["key"] = "Num7",
-				},
-			},
-			["name"] = "CAP 7",
-		},
-		["d3543pnilu3543cd23vd1vpnilvu0"] = {
-			["added"] = {
-				[1] = {
-					["key"] = "Num8",
-				},
-			},
-			["name"] = "CAP 8/Heading",
-		},
-		["d3544pnilu3544cd23vd1vpnilvu0"] = {
-			["added"] = {
-				[1] = {
-					["key"] = "Num9",
-				},
-			},
-			["name"] = "CAP 9",
-		},
-		["d3670pnilu3670cd38vd1vpnilvu0"] = {
-			["added"] = {
-				[1] = {
-					["key"] = "P",
-					["reformers"] = {
-						[1] = "RAlt",
-						[2] = "RCtrl",
-					},
-				},
-			},
-			["name"] = "LANTIRN S4 HAT Press (Declutter/Lase Auto-Man/MGC)",
-		},
-		["d3672pnilunilcd38vd0vpnilvunil"] = {
-			["added"] = {
-				[1] = {
-					["key"] = "Down",
-					["reformers"] = {
-						[1] = "RAlt",
-					},
-				},
-			},
-			["name"] = "LANTIRN Mode NAV - A/A ",
-		},
-		["d3672pnilunilcd38vd1vpnilvunil"] = {
-			["added"] = {
-				[1] = {
-					["key"] = "Up",
-					["reformers"] = {
-						[1] = "RAlt",
-					},
-				},
-			},
-			["name"] = "LANTIRN Mode A/G",
-		},
-		["d3676pnilunilcd38vd0.5vpnilvunil"] = {
-			["added"] = {
-				[1] = {
-					["key"] = "G",
-					["reformers"] = {
-						[1] = "LCtrl",
-					},
-				},
-			},
-			["name"] = "LANTIRN Power IMU",
-		},
-		["d3676pnilunilcd38vd0vpnilvunil"] = {
-			["added"] = {
-				[1] = {
-					["key"] = "B",
-					["reformers"] = {
-						[1] = "LCtrl",
-					},
-				},
-			},
-			["name"] = "LANTIRN Power OFF",
-		},
-		["d3676pnilunilcd38vd1vpnilvunil"] = {
-			["added"] = {
-				[1] = {
-					["key"] = "T",
-					["reformers"] = {
-						[1] = "LCtrl",
-					},
-				},
-			},
-			["name"] = "LANTIRN Power POD",
-		},
-		["d3682pnilu3682cd38vd-1vpnilvu0"] = {
-			["added"] = {
-				[1] = {
-					["key"] = "Left",
-					["reformers"] = {
-						[1] = "LAlt",
-					},
-				},
-			},
-			["name"] = "LANTIRN Slew Left",
-		},
-		["d3682pnilu3682cd38vd1vpnilvu0"] = {
-			["added"] = {
-				[1] = {
-					["key"] = "Right",
-					["reformers"] = {
-						[1] = "LAlt",
-					},
-				},
-			},
-			["name"] = "LANTIRN Slew Right",
-		},
-		["d3683pnilu3683cd38vd-1vpnilvu0"] = {
-			["added"] = {
-				[1] = {
-					["key"] = "Down",
-					["reformers"] = {
-						[1] = "LAlt",
-					},
-				},
-			},
-			["name"] = "LANTIRN Slew Down",
-		},
-		["d3683pnilu3683cd38vd1vpnilvu0"] = {
-			["added"] = {
-				[1] = {
-					["key"] = "Up",
-					["reformers"] = {
-						[1] = "LAlt",
-					},
-				},
-			},
-			["name"] = "LANTIRN Slew Up",
-		},
-		["d3691pnilunilcd38vd1vpnilvu0"] = {
-			["added"] = {
-				[1] = {
-					["key"] = "I",
-					["reformers"] = {
-						[1] = "RAlt",
-					},
-				},
-			},
-			["name"] = "LANTIRN OPER / STBY (Unstow)",
-		},
-		["d3708pnilu3708cd2vd1vpnilvu0"] = {
-			["added"] = {
-				[1] = {
-					["key"] = "K",
-					["reformers"] = {
-						[1] = "LShift",
-					},
-				},
-			},
-			["name"] = "Mic Button ICS - SRS Only / No Menu",
+			["name"] = "Mic Button UHF - SRS Only / No Menu",
 		},
 		["dnilp210u214cdnilvdnilvpnilvunil"] = {
 			["name"] = "View Up Right slow",
